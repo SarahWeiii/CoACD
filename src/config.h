8c977b000a6c1a9ec66fb8fb49c5318bb066496f
yetigit <baidhir.hidair@gmail.com>
2023-03-30 01:19:29 +0200

    update config

--- src/config.h
+++ src/config.h
@@ -57,7 +57,7 @@ namespace coacd
       seed = 1234;
       rv_k = 0.3;
       preprocess = true;
-      prep_resolution = 10000;
+      prep_resolution = 50;
       pca = false;
       merge = true;
       mani_plus = true;
