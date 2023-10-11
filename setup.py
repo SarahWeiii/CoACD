0871201dae8618a66a2d7c73bd99f542be6840dc
eliphatfs <flandre@scarletx.cn>
2023-10-10 20:24:56 -0700

    Change osx arch config.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class bdist_wheel_abi3(bdist_wheel):
 
         if python.startswith("cp"):
             # on CPython, our wheels are abi3 and compatible back to 3.6
-            return "cp36", "abi3", plat
+            return python, "abi3", plat
 
         return python, abi, plat
 
