379968bdf6180ed6c7149048222599713fbc9ecd
Xinyue Wei <13659185706@163.com>
2025-04-30 15:09:25 -0700

    1.0.7, fix interface params

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="coacd",
-    version="1.0.6",
+    version="1.0.7",
     author_email="xiwei@ucsd.edu",
     keywords="collision convex decomposition",
     description="Approximate Convex Decomposition for 3D Meshes with Collision-Aware Concavity and Tree Search",
