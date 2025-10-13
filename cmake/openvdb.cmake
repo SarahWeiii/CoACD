585b9767876e255b88f8d75dd82bc4c731057715
Kasper <kasper.lorenzen@gmail.com>
2025-10-14 00:01:00 +0200

    Create install target for CoACD (#85)
    * Made installable
    
    * removed formatting changes
    
    * removed more formatting changes
    
    ---------
    
    Co-authored-by: Xinyue Wei <13659185706@163.com>

--- cmake/openvdb.cmake
+++ cmake/openvdb.cmake
@@ -14,33 +14,79 @@ set(OPENVDB_ENABLE_UNINSTALL OFF CACHE BOOL "" FORCE)
 set(OPENVDB_FUTURE_DEPRECATION OFF CACHE BOOL "" FORCE)
 set(USE_BLOSC OFF CACHE STRING "" FORCE)
 set(USE_ZLIB OFF CACHE STRING "" FORCE)
+set(TBB_TEST OFF CACHE BOOL "" FORCE)
 
 include(FetchContent)
-FetchContent_Declare(
+if(CMAKE_VERSION VERSION_LESS 3.28)
+  FetchContent_Declare(
     openvdb
     GIT_REPOSITORY https://github.com/AcademySoftwareFoundation/openvdb.git
-    GIT_TAG        v8.2.0
-    PATCH_COMMAND cmake -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/cmake/openvdb_CMakeLists.txt <SOURCE_DIR>/openvdb/openvdb/CMakeLists.txt && cmake -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/cmake/openvdb_cmd_CMakeLists.txt <SOURCE_DIR>/openvdb/openvdb/cmd/CMakeLists.txt
-)
-
-FetchContent_Declare(
+    GIT_TAG v8.2.0
+    PATCH_COMMAND
+      cmake -E copy_if_different
+      ${CMAKE_CURRENT_SOURCE_DIR}/cmake/openvdb_CMakeLists.txt
+      <SOURCE_DIR>/openvdb/openvdb/CMakeLists.txt && cmake -E copy_if_different
+      ${CMAKE_CURRENT_SOURCE_DIR}/cmake/openvdb_cmd_CMakeLists.txt
+      <SOURCE_DIR>/openvdb/openvdb/cmd/CMakeLists.txt)
+  FetchContent_Declare(
     tbb
     GIT_REPOSITORY https://github.com/oneapi-src/oneTBB.git
-    GIT_TAG        v2022.0.0
-)
+    GIT_TAG v2022.0.0)
+
+else()
+
+  FetchContent_Declare(
+    openvdb
+    GIT_REPOSITORY https://github.com/AcademySoftwareFoundation/openvdb.git
+    GIT_TAG v8.2.0
+    PATCH_COMMAND
+      cmake -E copy_if_different
+      ${CMAKE_CURRENT_SOURCE_DIR}/cmake/openvdb_CMakeLists.txt
+      <SOURCE_DIR>/openvdb/openvdb/CMakeLists.txt && cmake -E copy_if_different
+      ${CMAKE_CURRENT_SOURCE_DIR}/cmake/openvdb_cmd_CMakeLists.txt
+      <SOURCE_DIR>/openvdb/openvdb/cmd/CMakeLists.txt EXCLUDE_FROM_ALL)
+  FetchContent_Declare(
+    tbb
+    GIT_REPOSITORY https://github.com/oneapi-src/oneTBB.git
+    GIT_TAG v2022.0.0
+    EXCLUDE_FROM_ALL)
+
+endif()
 
 set(CMAKE_CXX_FLAGS_OLD ${CMAKE_CXX_FLAGS})
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DTBB_ALLOCATOR_TRAITS_BROKEN")
-FetchContent_MakeAvailable(tbb)
+
+# Make tbb available
+if(CMAKE_VERSION VERSION_LESS 3.28)
+  FetchContent_GetProperties(tbb)
+  if(NOT tbb_POPULATED)
+    FetchContent_Populate(tbb)
+    add_subdirectory(${tbb_SOURCE_DIR} ${tbb_BINARY_DIR} EXCLUDE_FROM_ALL)
+  endif()
+else()
+  FetchContent_MakeAvailable(tbb)
+endif()
+
 set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS_OLD})
 
-if (WIN32)
+if(WIN32)
     set(OPENVDB_DISABLE_BOOST_IMPLICIT_LINKING OFF CACHE BOOL "" FORCE)
 endif()
 
 set(CMAKE_CXX_STANDARD 17)
 set(OPENVDB_ENABLE_UNINSTALL OFF CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(openvdb)
+
+# Make openvdb available
+if(CMAKE_VERSION VERSION_LESS 3.28)
+  FetchContent_GetProperties(openvdb)
+  if(NOT openvdb_POPULATED)
+    FetchContent_Populate(openvdb)
+    add_subdirectory(${openvdb_SOURCE_DIR} ${openvdb_BINARY_DIR} EXCLUDE_FROM_ALL)
+  endif()
+else()
+  FetchContent_MakeAvailable(openvdb)
+endif()
+
 set_target_properties(openvdb_static PROPERTIES POSITION_INDEPENDENT_CODE ON)
 
 target_include_directories(openvdb_static PUBLIC
--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -1,17 +1,38 @@
+find_package(ZLIB QUIET)
 
+if(ZLIB_FOUND)
+  message(STATUS "ZLIB found, skipping fetch.")
+  return()
+endif()
 include(FetchContent)
-FetchContent_Declare(
+
+if(CMAKE_VERSION VERSION_LESS 3.28)
+  FetchContent_Declare(
     zlib
     GIT_REPOSITORY https://github.com/madler/zlib.git
-    GIT_TAG        v1.2.11
-    OVERRIDE_FIND_PACKAGE
-)
+    GIT_TAG v1.2.11
+    OVERRIDE_FIND_PACKAGE)
 
-FetchContent_MakeAvailable(zlib)
+  FetchContent_GetProperties(zlib)
+  if(NOT zlib_POPULATED)
+    FetchContent_Populate(zlib)
+    add_subdirectory(${zlib_SOURCE_DIR} ${zlib_BINARY_DIR} EXCLUDE_FROM_ALL)
+  endif()
+else()
+
+  FetchContent_Declare(
+    zlib
+    GIT_REPOSITORY https://github.com/madler/zlib.git
+    GIT_TAG v1.2.11
+    EXCLUDE_FROM_ALL OVERRIDE_FIND_PACKAGE)
+  FetchContent_MakeAvailable(zlib)
+
+endif()
 
 set_target_properties(zlibstatic PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
 add_library(ZLIB::ZLIB ALIAS zlibstatic)
 set(ZLIB_LIBRARY ZLIB::ZLIB)
 set(ZLIB_INCLUDE_DIR ${zlib_SOURCE_DIR})
 set(ZLIB_FOUND TRUE)
-target_include_directories(zlibstatic PUBLIC ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
+target_include_directories(zlibstatic PUBLIC ${zlib_SOURCE_DIR}
+                                             ${zlib_BINARY_DIR})
